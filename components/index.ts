84dcf73220451254f8c7c0709d2d2d3801f115b9
emredkyc <emredkyc@hotmail.com>
2024-01-05 18:47:04 +0100

    export CarCard

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,7 @@ import NavBar from "./Navbar";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
 
 export {
     Hero,
@@ -14,4 +15,5 @@ export {
     SearchBar,
     CustomFilter,
     SearchManufacturer,
+    CarCard,
 }
