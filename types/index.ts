3816f2dd40a8806093f9901d5461b47a90068739
emredkyc <emredkyc@hotmail.com>
2024-01-05 13:49:03 +0100

    add assets

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+  }
