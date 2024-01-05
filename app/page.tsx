a7223bacd65fa86ac6acab9beb6f763b4f9dac19
emredkyc <emredkyc@hotmail.com>
2024-01-05 20:46:00 +0100

    delete title

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter />
+            <CustomFilter />
           </div>
         </div>
 
