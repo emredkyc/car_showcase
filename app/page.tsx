9f8232fcff46af5754e7dc390164f45cc42d059b
emredkyc <emredkyc@hotmail.com>
2024-01-06 20:33:50 +0100

    add ShowMore coponent and searchParams parameter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fetchCars } from "@/utils";
 import { HomeProps } from "@/types";
 import { fuels, yearsOfProduction } from "@/constants";
@@ -41,6 +41,11 @@ export default async function Home({ searchParams }: HomeProps) {
               <CarCard car={car} />
              ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ): (
           <div className="home__error-container">
