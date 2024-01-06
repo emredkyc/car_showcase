59edc2245e872041b28eb8dcf0072e83da9a4626
emredkyc <emredkyc@hotmail.com>
2024-01-06 20:31:51 +0100

    create ShowMore component

---
+++ components/ShowMore.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { ShowMoreProps } from "@/types";
+import { updateSearchParams } from "@/utils";
+import { CustomButton } from "@/components";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
+
+    // Update the "limit" search parameter in the URL with the new value
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathname);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  )
+}
+
+export default ShowMore
