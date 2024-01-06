bf7bf85a7bde620036e9157076bf2221412c111a
emredkyc <emredkyc@hotmail.com>
2024-01-06 14:04:17 +0100

    add searchParams

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
