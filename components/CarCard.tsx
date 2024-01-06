f57571b71f7b22279acc7908d2e0f032e56b8be1
emredkyc <emredkyc@hotmail.com>
2024-01-06 11:54:31 +0100

    add CarDetails component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { calculateCarRent } from "@/utils";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -65,6 +66,7 @@ const CarCard = ({ car }: CarCardProps) => {
         </div>
       </div>
 
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   );
 };
