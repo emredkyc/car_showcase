4849ab91daa3c1d915b9b59a34dc9663f8f744de
emredkyc <emredkyc@hotmail.com>
2024-01-06 14:01:31 +0100

    add generateCarImageUrl

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import CarDetails from "./CarDetails";
@@ -34,7 +34,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
 
       <div className='relative w-full h-40 my-3 object-contain'>
-        <Image src='/hero.png' alt='car model' fill priority className='object-contain' />
+        <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
       </div>
 
       <div className='relative flex w-full mt-2'>
@@ -71,4 +71,4 @@ const CarCard = ({ car }: CarCardProps) => {
   );
 };
 
-export default CarCard;
+export default CarCard
