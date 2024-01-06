046503aa8b78c293c937c42356c0d21b0e7e8cd7
emredkyc <emredkyc@hotmail.com>
2024-01-06 11:54:16 +0100

    export CarDetails component

--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,7 @@ import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 export {
     Hero,
@@ -16,4 +17,5 @@ export {
     CustomFilter,
     SearchManufacturer,
     CarCard,
+    CarDetails,
 }
